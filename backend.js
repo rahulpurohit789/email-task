49425f8a25c6ec6b136964eab79ec72dc8d6d96e
rahulpurohit789 <rahulpurohitrp789rp@gmail.com>
2024-01-24 21:44:09 +0530

    email

---
+++ backend.js
@@ -0,0 +1,61 @@
+import express from "express";
+import multer, { diskStorage } from "multer";
+import nodemailer from "nodemailer";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import { Console } from "console";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app=express();
+const port=3000;
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage , diskStorage });
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+  app.use(bodyParser.urlencoded({ extended: true}));
+
+  
+app.post  ("/mail",upload.single("file"),async(req,res)=>{
+    const {email,subject,composeMail}=req.body;
+    const updatedFile = req.file;
+    console.log(subject);
+    console.log(updatedFile);
+    const send = nodemailer.createTransport({
+        host:'smtp.gmail.com',
+        port:465,
+        secure:true,
+        auth:{
+            user:'virendrarp789rp@gmail.com',
+            pass:'atoi iska aftp hial'
+        }
+       
+        });
+        const info = await send.sendMail({
+            from:'<virendrarp789rp@gmail.com>',
+            to:[email],
+            subject:subject,
+            text:composeMail,
+            attachments: [
+                {
+                    filename: updatedFile.originalname,
+                    content: updatedFile.buffer,
+
+                }
+            ]
+            
+        })
+        
+        console.log("message sent:"+ info.messageId);
+        console.log("Accepted email"+info.accepted);
+        console.log("Rejected email"+info.rejected);
+        
+res.send("File uploaded successfully");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
